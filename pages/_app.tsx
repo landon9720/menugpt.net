894f79a5f3f040873dc4d0569dc1064eb712e43f
Landon Kuhn <landon9720@gmail.com>
2023-07-25 11:54:13 -0700

    add adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4003870517943315"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      ></Script>
     </>
   )
 }
