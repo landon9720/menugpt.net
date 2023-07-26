846b537a84f6e9e88432bcd7b8e1a29b451ce767
Landon Kuhn <landon9720@gmail.com>
2023-07-26 16:31:38 -0700

    tuning

--- lib/globals.ts
+++ lib/globals.ts
@@ -3,7 +3,6 @@ export const NEW_USER_CREDITS = 100
 export const BODY_SYSTEM_PROMPT = [
   'Be brief, informative, interesting, on-topic.',
   'Include puns, and non-sequiturs.',
-  'Never say "As an AI" or similar.',
   'Produce 1-2 short paragraphs with 1-3 short sentences.',
 ].join(' ')
 
@@ -11,5 +10,6 @@ export const SUGGESTION_SYSTEM_PROMPT = [
   'Provide a menu of 3-7 brief choices.',
   'Choices may be serious, practical, humorous, unexpected, or whimsical.',
   'Choices should be questions, request more detail, request a new subject, or provide information.',
+  'A choice is what the user may say next.',
   'The user will select a choice to continue this conversation.',
 ].join(' ')
