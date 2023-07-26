0a58905fec288fd9eeef0e0ba9d61bc80b920094
Landon Kuhn <landon9720@gmail.com>
2023-07-26 12:05:23 -0700

    seed revalidate

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,9 +1,13 @@
-import { setPrompt } from '@/lib/data'
+import { getPrompt, setPrompt } from '@/lib/data'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  await setPrompt('1', {
-    prompt: 'Existence',
-  })
+  const existing = await getPrompt('1')
+  if (!existing) {
+    await setPrompt('1', {
+      prompt: 'Existence',
+    })
+    res.revalidate('/prompt/1')
+  }
   res.status(200).end()
 }
