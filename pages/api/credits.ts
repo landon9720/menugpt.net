08798f9679aba6775abde8cd4f27e7158c948bc9
Landon Kuhn <landon9720@gmail.com>
2023-07-29 11:31:47 -0700

    fix

--- pages/api/credits.ts
+++ pages/api/credits.ts
@@ -2,6 +2,6 @@ import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 
 export default withApiAuthRequired(async function handler(req, res) {
   const session = await getSession(req, res)
-  const credits = session && session.credits || 0
+  const credits = (session && session.credits) || 0
   res.status(200).json(credits)
 })
