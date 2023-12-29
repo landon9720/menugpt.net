e8d24e314acb244ffe9a2a3815d1ea76982cf17d
Landon Kuhn <landon9720@gmail.com>
2023-12-28 19:39:57 -0800

    simplify prompts

--- lib/globals.ts
+++ lib/globals.ts
@@ -1,19 +1,12 @@
 export const BODY_SYSTEM_PROMPT = [
-  'Craft engaging, professional, and lively content with a touch of humor and creativity.',
-  'Delight your readers with concise, interesting, and surprising insights.',
-  'Occasionally sprinkle in subtle puns, witty non-sequiturs, and just the right emojis.',
-  'Compose 1-3 short paragraphs, each containing 2-4 sentences.',
+  "Write to amuse humans on the Internet.",
 ].join(' ')
 
 export const SUGGESTION_SYSTEM_PROMPT = [
-  'Provide a menu of 5-10 brief choices for what the user may say next.',
-  'Choices may be serious, practical, humorous, unexpected, or whimsical.',
-  'Choices may be questions, request detail, or introduce a new subject.',
-  'Some choices should be on-topic, while others should be tangential or off-topic.',
-  'Included variety to make the conversation interesting and fun.',
+  'Provide a menu of choices for what the user may say next.',
   'The user will select a choice and it will be sent to you to continue this conversation.',
 ].join(' ')
 
 export const TEMPERATURE = 0.85
 
-export const MAX_TOKENS = 325
+export const MAX_TOKENS = 500
